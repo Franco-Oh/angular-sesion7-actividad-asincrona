19ecaa97e78fd74728986dabf49a480a2289f77a
Franco Ojeda <fojedavideos@gmail.com>
2022-12-29 13:24:51 -0500

    agregar movimiento dado

--- src/app/elementos/dado/dado.component.ts
+++ src/app/elementos/dado/dado.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
+import { DOCUMENT } from '@angular/common'; 
 
 @Component({
   selector: 'app-dado',
@@ -7,15 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DadoComponent implements OnInit {
   numero:Number = 1;
-
   numRepetido:any;
+  temblar = false;
 
-  girarDado(){
-    this.numRepetido = this.numero;
+  numAzar(){
     this.numero = Math.round(Math.random() * 5 + 1);
     while (this.numero == this.numRepetido) {
       this.numero = Math.round(Math.random() * 5 + 1);
     }
+  }
+
+  girarDado(){
+    this.temblar = !this.temblar;
+    this.numRepetido = this.numero;
+    setTimeout(() => {
+      this.numAzar();
+    }, 50);
+    setTimeout(() => {
+      this.numAzar();
+    }, 100);
+    setTimeout(() => {
+      this.numAzar();
+    }, 150);
+    setTimeout(() => {
+      this.numAzar();
+    }, 200);
+    setTimeout(() => {
+      this.numAzar();
+    }, 250);
+    setTimeout(() => {
+      this.numAzar();
+    }, 300);
+    setTimeout(() => {     
+      this.numAzar();
+      this.temblar = !this.temblar;
+    }, 400);
   }
 
   constructor() { }
